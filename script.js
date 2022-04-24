5bd2716e715115804476b37eaa9cf0b53546d898
Alex <alexcannon_17@hotmail.com>
2022-04-24 04:26:18 -0400

    implemented variable resizing in js

--- script.js
+++ script.js
@@ -1,17 +1,25 @@
-const n = 16;
+let n = 16;
+mousedColor = "blue"
+const reDraw = true
 
 
-for (j = 0; j < n**2; j++) {
-    const div = document.createElement('div');
-    div.setAttribute("id", `Box${j}`)
-    div.setAttribute("class", `Box`)
-    gridBox.appendChild(div);
-    div.addEventListener('mouseover', () => mouseOvered(div.id))
-
-
+function drawGrid(n) {
+    for (j = 0; j < n**2; j++) {
+        const div = document.createElement('div');
+        div.setAttribute("id", `Box${j}`)
+        div.setAttribute("class", `Box`)
+        gridBox.appendChild(div);
+        div.addEventListener('mouseover', () => mouseOvered(div.id))
+    };
 };
 
 function mouseOvered(divId) {
-    console.log(divId);
-    document.getElementById(`${divId}`).style.backgroundColor = "white";
+    document.getElementById(`${divId}`).style.backgroundColor = `${mousedColor}`;
 }
+function resizeGrid(n) {
+    document.getElementById("gridBox").style.gridTemplateColumns = `repeat(${n}, 1fr)`;
+    document.getElementById("gridBox").style.gridAutoRows = `${960/n}px`
+    console.log(n)
+    drawGrid(n)
+}
+resizeGrid(100)
